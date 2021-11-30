8dda6340d883d4ebb207a7104c398cab7053c571
Geoff Clark <pinball.map@gmail.com>
2021-12-01 02:15:19 +0800

    Start of EQ background grid

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -233,6 +233,8 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll(Colours::black);
 
+    g.drawImage(background, getLocalBounds().toFloat());
+
     auto responseArea = getLocalBounds();
 
     auto w = responseArea.getWidth();
@@ -287,6 +289,41 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
     g.setColour(Colours::white);
     g.strokePath(responseCurve, PathStrokeType(2.f));
+}
+
+void ResponseCurveComponent::resized()
+{
+    using namespace juce;
+    background = Image(Image::PixelFormat::RGB, getWidth(), getHeight(), true);
+
+    Graphics g(background);
+
+    Array<float> freqs
+    {
+        20, 30, 40, 50, 100,
+        200, 300, 400, 500, 1000,
+        2000, 3000, 4000, 5000, 10000,
+        20000
+    };
+
+    g.setColour(Colours::white);
+    for (auto f : freqs)
+    {
+        auto normX = mapFromLog10(f, 20.f, 20000.f);
+
+        g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
+    }
+
+    Array<float> gain
+    {
+        -24, -12, 0, 12, 24
+    };
+
+    for (auto gDb : gain)
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(getHeight()), 0.f);
+        g.drawHorizontalLine(y, 0, getWidth());
+    }
 }
 
 //==============================================================================
